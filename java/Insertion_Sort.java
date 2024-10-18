533c05fbfc7faf762f4ed9a95b20a939d17d6de5
mythreyan007 <mythreyan6383@gmail.com>
2024-10-18 23:41:03 +0530

    Update Insertion_Sort.java
    cleared

--- java/Insertion_Sort.java
+++ java/Insertion_Sort.java
@@ -1,6 +1,9 @@
-public class Insertion_Sort {
+public class InsertionSort {
     public int[] sortArray(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
+        if (nums == null) {
+            return null; // Handle null input
+        }
+        for (int i = 1; i < nums.length; i++) {
             int key = nums[i];
             int j = i - 1;
             while (j >= 0 && nums[j] > key) {
@@ -10,5 +13,16 @@ public class Insertion_Sort {
             nums[j + 1] = key;
         }
         return nums;
+    }
+
+    public static void main(String[] args) {
+        InsertionSort sorter = new InsertionSort();
+        int[] nums = {5, 2, 9, 1, 5, 6};
+        int[] sortedArray = sorter.sortArray(nums);
+        
+        // Print sorted array
+        for (int num : sortedArray) {
+            System.out.print(num + " ");
+        }
     }
 }
